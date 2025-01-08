c67076aacfa04a49930a9fd9f3b1f2617e8f3258
Андрей Пряшников <prandruha2003@yandex.ru>
2025-01-08 17:24:25 +0300

    5 Fix

--- RegexToNFA/main.cpp
+++ RegexToNFA/main.cpp
@@ -34,10 +34,8 @@ int main(int argc, char *argv[]) {
 //    regularExpression = "(r|s)+(s)*";
 //    regularExpression = "(r|s)*|s";
 //    regularExpression = "(r|s)+s";
-    regularExpression = "(ARRAY|BEGIN|ELSE|END|IF|OF|OR|PROGRAM|PROCEDURE|THEN|TYPE|VAR)";
-
-
-    regularExpression = R"(ARRAY|BEGIN|ELSE|END|IF|OF|OR|PROGRAM|PROCEDURE|THEN|TYPE|VAR|\*|\+|-|/|;|,|\(|\)|[|]|=|>|<|<=|>=|<>|:|:=|.|(0|1|2|3|4|5|6|7|8|9)+.(0|1|2|3|4|5|6|7|8|9)+|(0|1|2|3|4|5|6|7|8|9)+|//)";
+//    regularExpression = "(ARRAY|BEGIN|ELSE|END|IF|OF|OR|PROGRAM|PROCEDURE|THEN|TYPE|VAR)";
+//    regularExpression = R"(ARRAY|BEGIN|ELSE|END|IF|OF|OR|PROGRAM|PROCEDURE|THEN|TYPE|VAR|\*|\+|-|/|;|,|\(|\)|[|]|=|>|<|<=|>=|<>|:|:=|.|(0|1|2|3|4|5|6|7|8|9)+.(0|1|2|3|4|5|6|7|8|9)+|(0|1|2|3|4|5|6|7|8|9)+|//)";
 
 //
 
