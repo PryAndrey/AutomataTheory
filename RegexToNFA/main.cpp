dd5d64956cbd8e6d89eac8e02d1379dec979104c
Андрей Пряшников <prandruha2003@yandex.ru>
2025-01-08 19:03:20 +0300

    Fix 5

--- RegexToNFA/main.cpp
+++ RegexToNFA/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "RegexToNFA.h"
 #include <codecvt>
-#include <windows.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3 && argc != 4) {
@@ -10,7 +9,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     std::setlocale(LC_ALL, "Russian");
-    SetConsoleOutputCP(CP_UTF8);
     std::string outputFile;
     std::string regularExpression;
 
